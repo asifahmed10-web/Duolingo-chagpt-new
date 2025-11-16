57fcc3bf3e4b262c9538c15bee3d7eef2a2bbb63
asifahmed10-web <asifislam572@gmail.com>
2025-11-17 01:19:42 +0600

    Delete new.js

--- new.js
+++
@@ -1,8 +0,0 @@
-async function loadWords() {
-    const response = await fetch("/vocab.json");
-    const data = await response.json();
-
-    // random 20 words only
-    return data.sort(() => Math.random() - 0.5).slice(0, 20);
-}
-
