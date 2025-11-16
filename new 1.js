568bd05507970b113154b2e6cf5cd7413969a945
asifahmed10-web <asifislam572@gmail.com>
2025-11-17 01:22:14 +0600

    Create new 1.js

---
+++ new 1.js
@@ -0,0 +1,7 @@
+async function loadWords() {
+    const response = await fetch("/vocab.json");
+    const data = await response.json();
+
+    // random 20 words only
+    return data.sort(() => Math.random() - 0.5).slice(0, 20);
+}
